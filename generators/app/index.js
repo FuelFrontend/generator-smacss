4127b267d35f3a8de1921ad42b27eeb0b61a3ab6
logeshpaul <logeshpaul@gmail.com>
2015-01-27 13:41:37 +0530

    Minor formatting update

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,10 +1,10 @@
 'use strict';
 var yeoman = require('yeoman-generator'),
-		fs = require('fs'),
-		util = require('util'),
-		path = require('path'),
-		yosay = require('yosay'),
-		chalk = require('chalk');
+	fs = require('fs'),
+	util = require('util'),
+	path = require('path'),
+	yosay = require('yosay'),
+	chalk = require('chalk');
 
 var smacssGenerator = yeoman.generators.Base.extend({
 
@@ -58,72 +58,72 @@ var smacssGenerator = yeoman.generators.Base.extend({
 	    var done = this.async();
 
 	    if (!this.options['skip-welcome-message']) {
-	      // Welcome Message
-				this.log(yosay('Yo! Welcome to SMACSS'));
-				console.log(chalk.magenta.bold('You\'re using the perfectionist generator for frontend.'));
-				console.log(chalk.gray('================================================================'));
-				console.log(chalk.gray('Answer (3) simple questions to kick start your project'));
+	    	// Welcome Message
+			this.log(yosay('Yo! Welcome to SMACSS'));
+			console.log(chalk.magenta.bold('You\'re using the perfectionist generator for frontend.'));
+			console.log(chalk.gray('================================================================'));
+			console.log(chalk.gray('Answer (3) simple questions to kick start your project'));
 	    }
 
 	    var prompts = [{
-	      name: 'appName',
-	      message: 'What would you like to name your app/site?',
-	      default: 'New Project'
+			name: 'appName',
+			message: 'What would you like to name your app/site?',
+			default: 'New Project'
 	    },{
-				name: 'appType',
-				message: 'Kind of app/site you are trying to build?',
-				type: 'list',
-				choices:[{
-						name: 'Static HTML',
-						value: 'includeStaticApp',
-						checked: false
-		     	},{
-		        name: 'Angular App',
-		        value: 'includeAngularApp',
-		        checked: false
-		      },{
-		        name: 'Ember App',
-		        value: 'includeEmberApp',
-		        checked: false
-		      },{
-		        name: 'Mobile App',
-		        value: 'includeMobileApp',
-		        checked: false
-		    }],
-	      default: 1
+			name: 'appType',
+			message: 'Kind of app/site you are trying to build?',
+			type: 'list',
+			choices:[{
+					name: 'Static HTML',
+					value: 'includeStaticApp',
+					checked: false
+	     		},{
+			        name: 'Angular App',
+			        value: 'includeAngularApp',
+			        checked: false
+		      	},{
+			        name: 'Ember App',
+			        value: 'includeEmberApp',
+			        checked: false
+		      	},{
+			        name: 'Mobile App',
+			        value: 'includeMobileApp',
+			        checked: false
+	    	}],
+	      	default: 1
 	    },{
-				name: 'appFeatures',
-				message: 'How about some additional features',
-				type: 'checkbox',
-				choices:[{
-					  name: 'Modernizr',
-					  value: 'includeModernizr',
-					  checked: false
-					},{
-					  name: 'jQuery',
-					  value: 'includeQuery',
-					  checked: false
-					},{
-					  name: 'Respond',
-					  value: 'includeRespond',
-					  checked: false
-				}]
+			name: 'appFeatures',
+			message: 'How about some additional features',
+			type: 'checkbox',
+			choices:[{
+					name: 'Modernizr',
+					value: 'includeModernizr',
+					checked: false
+				},{
+					name: 'jQuery',
+					value: 'includeQuery',
+					checked: false
+				},{
+					name: 'Respond',
+					value: 'includeRespond',
+					checked: false
+			}]
 	    }];
 
 	    this.prompt(prompts, function (props) {
-	    	this.appName = props.appName;
-	    	this.appType = props.appType;
-	    	this.appFeatures = props.appFeatures;
-	    	this.response = props.response;
+    	this.appName = props.appName;
+    	this.appType = props.appType;
+    	this.appFeatures = props.appFeatures;
+    	this.response = props.response;
         this.taskrunner = props.taskrunner;
         this.csspreprocessor = props.csspreprocessor;
 
-	    	this.log(chalk.gray('================================================================'));
-	    	this.log(chalk.gray('Creating the project for you, please wait...'));
+    	this.log(chalk.gray('================================================================'));
+    	this.log(chalk.gray('Creating the project for you, please wait...'));
 
-	    	/*
-	    	var hasFeature = function (feat) {
-              return props.features.indexOf(feat) !== -1;
+    	/*
+    	var hasFeature = function (feat) {
+          return props.features.indexOf(feat) !== -1;
         };
 
         this.includeRespondJS = hasFeature('includeRespondJS');
@@ -133,46 +133,46 @@ var smacssGenerator = yeoman.generators.Base.extend({
         this.includeModernizr = hasFeature('includeModernizr');
         */
 
-	      done();
+	    done();
 	    }.bind(this));
 	},
 
 	app: function() {
 		this.mkdir(this.appName);
 		this.mkdir(this.appName + '/app');
-    this.mkdir(this.appName + '/app/css');
-    this.mkdir(this.appName + '/app/js');
-    this.mkdir(this.appName + '/app/fonts');
-    this.mkdir(this.appName + '/app/img');
-    this.log(chalk.green('Your project is created, cd to your project to-do more!'));
+		this.mkdir(this.appName + '/app/css');
+		this.mkdir(this.appName + '/app/js');
+		this.mkdir(this.appName + '/app/fonts');
+		this.mkdir(this.appName + '/app/img');
+		this.log(chalk.green('Your project is created, cd to your project to-do more!'));
 	},
 
 	helper: function () {
-  	//console.log('Need some Help?');
+		//console.log('Need some Help?');
 	},
 
 	errorHanding: function () {
-    //this.log('Something has gone wrong!');
+		//this.log('Something has gone wrong!');
 	},
 
 	install: function () {
 		//generator.installDependencies();
-    //this.spawnCommand('composer', ['install']);
-  },
+		//this.spawnCommand('composer', ['install']);
+  	},
 
-  paths: function () {
-    //var destinationRoot = this.destinationRoot();
-    //this.log(destinationRoot);
-    // returns '~/projects'
+	paths: function () {
+		//var destinationRoot = this.destinationRoot();
+		//this.log(destinationRoot);
+		// returns '~/projects'
 
-    // this.destinationPath('index.js');
-    // returns '~/projects/index.js'
-  },
+		// this.destinationPath('index.js');
+		// returns '~/projects/index.js'
+	},
 
-  projectfiles: function() {
-    // this.copy('editorconfig', '.editorconfig');
-    // this.copy('jshintrc', '.jshintrc');
-  }
+	projectfiles: function() {
+		// this.copy('editorconfig', '.editorconfig');
+		// this.copy('jshintrc', '.jshintrc');
+	}
 });
 
 module.exports = smacssGenerator;
