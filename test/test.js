c75745636f44214982e088e06b23c4d31069c418
logeshpaul <logeshpaul@gmail.com>
2015-04-08 18:29:30 +0530

    Tests, Travis CI enabled, Hound - Config

---
+++ test/test.js
@@ -0,0 +1,50 @@
+/*global describe, beforeEach, it */
+// TODO: Add tests for all app types
+'use strict';
+var path = require('path');
+var helpers = require('yeoman-generator').test;
+
+describe('Generator Smacss test', function () {
+  beforeEach(function (done) {
+    helpers.testDirectory(path.join(__dirname, 'temp'), function (err) {
+      if (err) {
+        done(err);
+        return;
+      }
+
+      this.smacss = helpers.createGenerator('smacss:app', [
+        '../../app', [
+          helpers.createDummyGenerator(),
+          'mocha:app'
+        ]
+      ]);
+      this.smacss.options['skip-install'] = true;
+
+      done();
+    }.bind(this));
+  });
+
+  it('the generator can be required without throwing', function () {
+    // not testing the actual run of generators yet
+    this.app = require('../app');
+  });
+
+  it('creates expected files', function (done) {
+    var expected = [
+      'package.json',
+      'gulpfile.js',
+      'app/favicon.ico',
+      'app/index.html'
+    ];
+
+    helpers.mockPrompt(this.smacss, {
+      appType: ['typeFullPackWebApp'],
+      appFeatures: ['includeQuery']
+    });
+
+    this.smacss.run(function () {
+      helpers.assertFile(expected);
+      done();
+    });
+  });
+});
